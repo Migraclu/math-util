701f5e339a5e4e07979b757e4f2fd4e20edb72b6
Migraclu <luuthanhhuy307@gmail.com>
2022-06-15 11:56:56 +0700

    add regression test

--- src/com/huylt/mathutil/core/MathUtil.java
+++ src/com/huylt/mathutil/core/MathUtil.java
@@ -24,19 +24,40 @@ public class MathUtil {
     // 21 giai thừa tràn kiểu long
     // bài này quy ước tính n! trong khoảng từ 0..20
     
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if (n <0 || n>20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 to 20");
+//        
+//        if (n== 0 || n == 1){
+//            return 1; // kết thúc cuộc chơi sớm nếu nhận nhngwx đầu vào đặc biệt
+//            
+//        }
+//        
+//        long product = 1; // tích nhân dồn,
+//        for (int i = 1; i <= n; i++) {
+//            product *=i;
+//        }
+//        return product;
+//    }
+//    
+    
+    // Học đệ quy trong vòng 30x -RECURSION
+    // hiện tượng gọi lại chính mình với 1 quy mô khác
+    // ví dụ: con búp bê Nga, giống nhau và lồng trong nhau
+    // búp bê to, nhỏ hơn, nhỏ hơn mữa,.. đến điểm dừng
+    
+    // tính dùm tui 6!
+    //n! = 1.2.3.4.5.6.....n
+    // 6! =6*5!
+    
+    public static long getFactorial (int n){
         if (n <0 || n>20)
             throw new IllegalArgumentException("Invalid argument. N must be between 0 to 20");
         
         if (n== 0 || n == 1){
             return 1; // kết thúc cuộc chơi sớm nếu nhận nhngwx đầu vào đặc biệt
-            
         }
         
-        long product = 1; // tích nhân dồn,
-        for (int i = 1; i <= n; i++) {
-            product *=i;
-        }
-        return product;
+        return n*getFactorial(n-1);
     }
 }
